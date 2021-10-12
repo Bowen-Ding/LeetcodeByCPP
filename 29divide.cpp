cba5dd03f7eb9c004a4192e5ba4ec3286e59a1b9
DingBowen <1062682058@qq.com>
2021-10-12 12:01:09 +0800

    29 两数相除

---
+++ 29divide.cpp
@@ -0,0 +1,72 @@
+#include <bits//stdc++.h>
+#include <vector>
+using namespace std;
+
+/*
+题目描述
+给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+
+返回被除数 dividend 除以除数 divisor 得到的商。
+
+整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+
+ 
+
+示例 1:
+
+输入: dividend = 10, divisor = 3
+输出: 3
+解释: 10/3 = truncate(3.33333..) = truncate(3) = 3
+示例 2:
+
+输入: dividend = 7, divisor = -3
+输出: -2
+解释: 7/-3 = truncate(-2.33333..) = -2
+*/
+
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        // 考虑被除数为最小值的情况
+        if (dividend == INT_MIN) {
+            if (divisor == 1) {
+                return INT_MIN;
+            }
+            if (divisor == -1) {
+                return INT_MAX;
+            }
+        }
+        // 考虑除数为最小值的情况
+        if (divisor == INT_MIN) {
+            return dividend == INT_MIN ? 1 : 0;
+        }
+        // 考虑被除数为 0 的情况
+        if (dividend == 0) {
+            return 0;
+        }
+
+        bool signal = false;
+        if (dividend > 0) {
+            dividend = -dividend;
+            signal = !signal;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+            signal = !signal;
+        }
+        vector<int> candidates = {divisor};
+        // 注意溢出
+        while (candidates.back() >= dividend - candidates.back()) {
+            candidates.push_back(candidates.back() + candidates.back());
+        }
+        int ans = 0;
+        for (int i = candidates.size() - 1; i >= 0; --i) {
+            if (candidates[i] >= dividend) {
+                ans += (1 << i);
+                dividend -= candidates[i];
+            }
+        }
+        return signal ? -ans : ans;
+    }
+};
