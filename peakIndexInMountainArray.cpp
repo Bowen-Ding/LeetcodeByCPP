8b3a04fd61412437cac6202439754714c62c2c44
DingBowen <1062682058@qq.com>
2021-10-14 11:13:54 +0800

    山脉数组

---
+++ peakIndexInMountainArray.cpp
@@ -0,0 +1,47 @@
+
+#include <bits//stdc++.h>
+#include <vector>
+using namespace std;
+
+/*
+题目描述
+符合下列属性的数组 arr 称为 山峰数组（山脉数组） ：
+
+arr.length >= 3
+存在 i（0 < i < arr.length - 1）使得：
+arr[0] < arr[1] < ... arr[i-1] < arr[i]
+arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+给定由整数组成的山峰数组 arr ，返回任何满足 arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1] 的下标 i ，即山峰顶部。
+
+ 
+
+示例 1：
+
+输入：arr = [0,1,0]
+输出：1
+
+示例 2：
+
+输入：arr = [24,69,100,99,79,78,67,36,26,19]
+输出：2
+*/
+
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int left = 1, right = n - 2, ans = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                ans = mid;
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
